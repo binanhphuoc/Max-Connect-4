697d0dacf918fc87a450d721a333ead5f8b0bbe9
Phuoc Do <binanhphuoc@gmail.com>
2018-02-28 21:41:38 -0600

    Initial Commit

---
+++ Connect4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Connect4
+//
+//  Created by Phuoc Do on 2/28/18.
+//  Copyright © 2018 Phuoc Do. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
